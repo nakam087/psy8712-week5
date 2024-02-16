3f8d1be853e3f5e57f61d2b31d53cb7f3ce73ed2
Sarah C Nakamoto <nakam087@umn.edu>
2024-02-16 17:57:35 -0600

    i think im done but not sure if i used the right join func, will ask others

--- R/week5.R
+++ R/week5.R
@@ -9,3 +9,16 @@ Bdata_tbl <- read_delim("../data/Bparticipants.dat", col_names = c("casenum", "p
 Bnotes_tbl <- read_delim("../data/Bnotes.txt", col_names=T)
 
 # Data Cleaning
+Aclean_tbl <- Adata_tbl %>%
+  separate(col=qs, paste0("q",1:5)) %>%
+  mutate(datadate = mdy_hms(datadate)) %>%
+  mutate(across(contains("q"), function(x) as.integer(x))) %>%
+  inner_join(Anotes_tbl, by="parnum") %>%
+  filter(is.na(notes))
+ABclean_tbl <-Bdata_tbl %>%
+  mutate(datadate=mdy_hms(datadate)) %>%
+  mutate(across(contains("q"), function(x) as.integer(x))) %>%
+  inner_join(Bnotes_tbl, by = "parnum") %>%
+  filter(is.na(notes)) %>%
+  bind_rows(Aclean_tbl, .id = "lab") %>% #can I do this with full join?
+  select(-notes)
