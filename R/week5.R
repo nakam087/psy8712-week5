352beceb2dc66342644faa0ecb7d41c1bb0fc860
Sarah C Nakamoto <nakam087@umn.edu>
2024-02-16 17:13:06 -0600

    imported and named files

---
+++ R/week5.R
@@ -0,0 +1,11 @@
+# Script Settings and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
+
+# Data Import
+Adata_tbl <- read_delim("../data/Aparticipants.dat", delim="-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
+Anotes_tbl <- read_csv("../data/Anotes.csv", col_names=T)
+Bdata_tbl <- read_delim("../data/Bparticipants.dat", col_names = c("casenum", "parnum", "stimver", "datadate", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10"))
+Bnotes_tbl <- read_delim("../data/Bnotes.txt", col_names=T)
+
+# Data Cleaning
